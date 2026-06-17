7d99e90aef1b92ebc0632c74a4c4e842dbdd771c
Aishu04082006 <1dt24cs015@dsatm.edu.in>
2026-06-17 12:10:53 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .models import Patient
+from .ml_models import predict_cancer
+
+
+def home(request):
+
+    result = None
+    confidence = None
+    patient = None
+
+    if request.method == 'POST':
+
+        name = request.POST['name']
+        age = request.POST['age']
+        gender = request.POST['gender']
+        image = request.FILES['image']
+
+        # Prediction
+        result, confidence = predict_cancer(image)
+
+        # Save to SQLite database
+        patient = Patient.objects.create(
+            name=name,
+            age=age,
+            gender=gender,
+            image=image,
+            prediction=result,
+            confidence=confidence
+        )
+
+    # Get all previous records
+    patients = Patient.objects.all().order_by('-date')
+# test commit 
+
+    return render(request, 'main/index.html', {
+        'result': result,
+        'confidence': confidence,
+        'patient': patient,
+        'patients': patients
+    })
